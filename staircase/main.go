23c08219b87ec9579576fcf3b1cccde0f8a690f5
Fadrian <fadrian.merdhianto@ruma.co.id>
2020-05-29 22:28:55 +0700

    staircase

---
+++ staircase/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the staircase function below.
+func staircase(n int32) {
+	for i := int(n)-1; i >= 0; i-- {
+		for j := 0; j < i; j++ {
+			fmt.Print(" ")
+		}
+		for j := i; j < int(n); j++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
+}
+
+// https://www.hackerrank.com/challenges/staircase
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	staircase(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
